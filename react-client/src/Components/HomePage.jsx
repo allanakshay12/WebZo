befe66cfcd8150e606b36353dbdd8afcac7a948b
allanakshay12 <allanakshay@outlook.com>
2019-03-17 12:48:43 +0530

    Stuff

--- react-client/src/Components/HomePage.jsx
+++ react-client/src/Components/HomePage.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Carousel from './CarouselComponent/Carousel';
 
 class HomePage extends Component {
   render() {
     return (
       <div className="Page-Content">
         
-        Content
+        <Carousel />
         
       </div>
     )
